bb58f960f16e85ee3ef9ff803f3ccee3dfb1f51f
Roshni <roshnistephen4@gmail.com>
2025-11-05 13:04:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e){
+    e.preventDefault();
+    alert('Thank you for contacting SOOJI_NOOLUM! This is a demo form.');
+    });
